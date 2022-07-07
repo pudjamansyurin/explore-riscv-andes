38ee9cb3c2bc32c2da9c589089e6d8ad7ba3ffd7
Puja Kusuma <pujakusumae@hotmail.com>
2022-07-07 10:28:21 +0700

    Change data type from U16 to U8

--- src/demo/main.c
+++ src/demo/main.c
@@ -18,12 +18,26 @@
 #define SPI_BITRATE		0
 #endif
 #define SPI_DATA_SIZE	255
+#define SPI_DUMMY_SIZE	2
+#define SPI_DUMMY_HEX	0x55
 
 extern void delay(uint64_t ms);
 static volatile char gpio_pressed = 0;
 
-uint16_t u16_txBuffer[SPI_DATA_SIZE];
-uint16_t u16_rxBuffer[SPI_DATA_SIZE];
+uint8_t u8_txBuffer[SPI_DATA_SIZE];
+uint8_t u8_rxBuffer[SPI_DATA_SIZE];
+uint8_t u8_outBuffer[SPI_DATA_SIZE+SPI_DUMMY_SIZE];
+
+void transform_data(uint8_t *u8_dst, const uint8_t *u8_src, uint16_t u16_len) {
+	uint16_t u16_i;
+
+	u8_dst[0] = SPI_DUMMY_HEX;
+	u8_dst[1] = SPI_DUMMY_HEX;
+
+	for(u16_i=0; u16_i<u16_len; u16_i++) {
+		u8_dst[u16_i+SPI_DUMMY_SIZE] = u8_src[u16_i];
+	}
+}
 
 void error_handler() {
     segment_write(0, 7);
@@ -38,7 +52,7 @@ void gpio_callback(uint32_t event) {
 int main(void)
 {
 	uint8_t u8_counter = 0;
-	uint16_t u16_i;
+	uint8_t u8_i;
 
 	// Initialize standard output
 	uart_init(38400);
@@ -52,56 +66,45 @@ int main(void)
 	}
 
 	// Generate data
-	for (u16_i = 0; u16_i < SPI_DATA_SIZE; u16_i++) {
-		if (0 == u16_i%2)
-			u16_txBuffer[u16_i] = 0xAA;
-		else
-			u16_txBuffer[u16_i] = 0xCC;
+	for (u8_i = 0; u8_i < SPI_DATA_SIZE; u8_i++) {
+		u8_txBuffer[u8_i] = u8_i;
 	}
 
 	while(1) {
-#if 1
 #ifdef SPI_MASTER
 		while(0 == gpio_pressed)
 		{};
 		gpio_pressed = 0;
 
-		if (afe_spi_transmit((uint8_t*) u16_txBuffer, sizeof(u16_txBuffer)) != NDS_DRIVER_OK) {
+		transform_data(u8_outBuffer, u8_txBuffer, sizeof(u8_txBuffer));
+
+		if (afe_spi_transmit(u8_outBuffer, sizeof(u8_outBuffer)) != NDS_DRIVER_OK) {
 			error_handler();
 		}
 #else
-		if (afe_spi_receive((uint8_t*) u16_rxBuffer, sizeof(u16_rxBuffer)) != NDS_DRIVER_OK) {
-			error_handler();
-		}
-#endif
-#else
-		if (afe_spi_transfer((uint8_t*) u16_txBuffer, (uint8_t*) u16_rxBuffer, sizeof(u16_txBuffer)) != NDS_DRIVER_OK) {
+		if (afe_spi_receive(u8_rxBuffer, sizeof(u8_rxBuffer)) != NDS_DRIVER_OK) {
 			error_handler();
 		}
 #endif
 
-#ifndef SPI_MASTER
+#ifdef SPI_MASTER
+        segment_write(0, u8_counter++);
+#else
         uint8_t u8_ok = 1;
 
-        // compare data
-        for (u16_i = 0; u16_i < SPI_DATA_SIZE; u16_i++) {
-            if (u16_rxBuffer[u16_i] != u16_i) {
-                u8_ok = 0;
-                break;
-            }
-        }
+		for (u8_i = 0; u8_i < SPI_DATA_SIZE; u8_i++) {
+			if (u8_rxBuffer[u8_i] != u8_i) {
+				u8_ok = 0;
+			}
+		}
 
-        // indicator
         if (u8_ok) {
             segment_write(0, u8_counter++);
         }
 
-        // clear data
-        for (u16_i = 0; u16_i < SPI_DATA_SIZE; u16_i++) {
-        	u16_rxBuffer[u16_i] = 0;
-        }
-#else
-        segment_write(0, u8_counter++);
+		for (u8_i = 0; u8_i < SPI_DATA_SIZE; u8_i++) {
+			u8_rxBuffer[u8_i] = 0;
+		}
 #endif
 	}
 
