86222fddad4b67c6993d9ed1fd41748f43519ecd
Puja Kusuma <pujakusumae@hotmail.com>
2022-07-26 11:17:10 +0700

    Added multiple timer evaluation

--- src/demo/main.c
+++ src/demo/main.c
@@ -4,98 +4,69 @@
  *
  */
 #include "ae210p.h"
-
+#include "timer.h"
 #include "../nds-stdout/uart.h"
-#include "../nds-7segment/segment.h"
 
-#include "../afe/afe.h"
-#ifdef USE_TRANSPORT_SPI
-#include "../afe/afe_spi.h"
-const sAfeTransport_t *Transport = &SPI_Transport;
-#else
-#include "../afe/afe_i2c.h"
-const sAfeTransport_t *Transport = &I2C_Transport;
-#endif
+//#define TICK_HZ         1
+//#ifdef CFG_SIMU
+// #define SIMU_FACTOR    (4)
+//#else
+// #define SIMU_FACTOR    (0)
+//#endif
 
-#define DATA_SIZE	255
+#define TIMER1          0
+#define TIMER2          1
+#define TIMER3          2
 
 extern void delay(uint64_t ms);
-static volatile char gpio_pressed = 0;
 
-static uint8_t u8_buffer[DATA_SIZE];
-
-void error_handler() {
-    segment_write(0, 7);
-    segment_write(1, 7);
+void error_handler()
+{
+	printf("error: %s, %d\r\n", __FILE__, __LINE__);
     while(1){};
 }
 
-void gpio_callback(uint32_t event) {
-	gpio_pressed = 1;
+void timer_irq_handler(void)
+{
+	if (timer_irq_status(TIMER1)) {
+		timer_irq_clear(TIMER1);
+		printf("TIM1");
+	} else if (timer_irq_status(TIMER2)) {
+		timer_irq_clear(TIMER2);
+		printf("TIM2");
+	} else if (timer_irq_status(TIMER3)) {
+		timer_irq_clear(TIMER3);
+		printf("TIM3");
+	}
+}
+
+void timer_configure(unsigned int tmr, unsigned int ms) {
+	unsigned int cycles;
+
+	cycles = msec_to_tick(ms);
+	timer_set_period(tmr, cycles);
+	timer_irq_enable(tmr);
 }
 
 int main(void)
 {
-	uint8_t u8_counter = 0;
-	uint8_t u8_i;
-
-	// Initialize standard output
 	uart_init(38400);
+	timer_init();
 
-	// Initialize seven segment
-	segment_init(gpio_callback);
+	timer_configure(TIMER1, 1000);
+	timer_start(TIMER1);
 
-	// Initialize SPI
-	if (Transport->init() != NDS_DRIVER_OK) {
-		error_handler();
-	}
+	timer_configure(TIMER2, 500);
+	timer_start(TIMER2);
 
-#ifdef USE_DIR_TRANSMIT
-	// Generate data
-	for (u8_i = 0; u8_i < DATA_SIZE; u8_i++) {
-		u8_buffer[u8_i] = u8_i;
-	}
-#endif
+	timer_configure(TIMER3, 250);
+	timer_start(TIMER3);
+
+	printf("hello world\r\n");
 
 	while(1) {
-#ifdef USE_NODE_MASTER
-		while(0 == gpio_pressed) {} ;
-		gpio_pressed = 0;
-#endif
-
-#ifdef USE_DIR_TRANSMIT
-		if (Transport->transmit(u8_buffer, sizeof(u8_buffer)) != NDS_DRIVER_OK) {
-			error_handler();
-		}
-#else
-		if (Transport->receive(u8_buffer, sizeof(u8_buffer)) != NDS_DRIVER_OK) {
-			error_handler();
-		}
-#endif
-
-#ifdef USE_DIR_TRANSMIT
-        segment_write(0, u8_counter++);
-#else
-        uint8_t u8_ok = 1;
-
-		for (u8_i = 0; u8_i < DATA_SIZE; u8_i++) {
-			if (u8_buffer[u8_i] != u8_i) {
-				u8_ok = 0;
-			}
-		}
-
-		for (u8_i = 0; u8_i < DATA_SIZE; u8_i++) {
-			u8_buffer[u8_i] = 0;
-		}
-
-        if (u8_ok) {
-            segment_write(0, u8_counter++);
-        }
-#endif
-
-#ifdef USE_NODE_MASTER
-        delay(100);
-#endif
+		printf(".");
+		delay(10);
 	}
 
 	return 0;
